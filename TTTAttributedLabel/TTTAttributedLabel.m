a808e6f962875f3c40b6c0782b5f0d2140ed37f0
Charlie Savage <cfis@zerista.com>
2013-05-07 00:30:01 -0600

    Once again take out async setting up of links since it causes crashes - not sure where last commit fixing this went.

--- TTTAttributedLabel/TTTAttributedLabel.m
+++ TTTAttributedLabel/TTTAttributedLabel.m
@@ -780,16 +780,16 @@ static inline NSAttributedString * NSAttributedStringBySettingColorFromContext(N
 
     self.links = [NSArray array];
     if (self.attributedText && self.dataDetectorTypes != UIDataDetectorTypeNone) {
-        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        //dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
             NSArray *results = [self.dataDetector matchesInString:[text string] options:0 range:NSMakeRange(0, [text length])];
             if ([results count] > 0) {
-                dispatch_async(dispatch_get_main_queue(), ^{
+                //dispatch_async(dispatch_get_main_queue(), ^{
                     if ([[self.attributedText string] isEqualToString:[text string]]) {
                         [self addLinksWithTextCheckingResults:results attributes:self.linkAttributes];
                     }
-                });
+                //});
             }
-        });
+        //});
     }
         
     [super setText:[self.attributedText string]];
